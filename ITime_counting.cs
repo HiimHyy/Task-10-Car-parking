3d1b4ac0dc8426c64e36e48a77addc29a813a129
HiimHyy <quochuytran130804@gmail.com>
2022-10-18 15:10:33 +0300

    Add project files.

---
+++ ITime_counting.cs
@@ -0,0 +1,8 @@
+﻿namespace Task_10_Car_parking
+{
+    internal interface ITime_counting
+    {
+        double endTime { get; set; }
+        double startTime { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Task_10_Car_parking;
+bool stillMore;
+double startTime, endTime;
+do
+{
+    Console.Write("When do you want to begin: ");
+    string received = Console.ReadLine();
+    while (!Double.TryParse(received, out startTime))
+    {
+        Console.Write("Invalid time, try again: ");
+        received = Console.ReadLine();
+    }
+    
+    Console.Write("When do you want to end: ");
+    received = Console.ReadLine();
+    while (!Double.TryParse(received, out endTime))
+    {
+        Console.Write("Invalid time, try again: ");
+        received = Console.ReadLine();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    Console.Write("Calculate another (Y/N): ");
+    string decision = Console.ReadLine().ToUpper();
+    if (decision.StartsWith("Y"))
+        stillMore = true;
+    else
+        stillMore = false;
+} while (stillMore);
