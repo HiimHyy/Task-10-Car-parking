4204999d4ac4aeb183ffe89dc3ab7adda55e63b9
HiimHyy <quochuytran130804@gmail.com>
2022-10-24 16:15:05 +0300

    Add price tab;le

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Globalization;
 using Task_10_Car_parking;
-bool stillMore;
+bool More;
 TimeOnly startTime, endTime;
 
 Console.WriteLine("-----------------------------------------------------------------------------");
-Console.WriteLine("Welcome to Leonardo Corporations, we are here to take money from your parking");
-Console.WriteLine("                Our services available from 7:00 to 22:00                    ");
+Console.WriteLine("Welcome to Huy Corporations, we are here to take money from your parking");
+Console.WriteLine("               -Our services available from 7:00 to 22:00-                   ");
+Console.WriteLine("            <2hr: 2€/hr | >2hr <=5hr: 1.75€/hr | >5hr: 1.5€/hr               ");
 Console.WriteLine("-----------------------------------------------------------------------------\n");
 do
 {
@@ -13,7 +14,7 @@ do
     string received = Console.ReadLine();
     while (!TimeOnly.TryParse(received, out startTime) || (startTime.Hour < 7 ) || (startTime.Hour > 22 ) || (startTime.Hour >= 22 && startTime.Minute > 00))
     {
-        Console.Write("Invalid time, available after 7:00 : ");
+        Console.Write("Invalid time, available after 7:00: ");
         received = Console.ReadLine();
     }
 
@@ -25,10 +26,9 @@ do
         received = Console.ReadLine();
     }
 
+    
     Time_counting fees = new Time_counting(startTime, endTime);
-    Console.WriteLine("Your car parking charges is: {0:F2} euros \n", fees.calculateCharges());
-    Console.ReadLine();
-
+    Console.WriteLine("Your car parking charges is: {0:F2} € \n", fees.calculateCharges());
 
 
 
@@ -36,7 +36,7 @@ do
     Console.Write("Calculate another (Y/N): ");
     string decision = Console.ReadLine().ToUpper();
     if (decision.StartsWith("Y"))
-        stillMore = true;
+        More = true;
     else
-        stillMore = false;
-} while (stillMore);
+        More = false;
+} while (More);
